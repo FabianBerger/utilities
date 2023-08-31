b5dc5b7dffa4e923bdabe3af4fc86ff43e17bad8
Fabian Berger <fb593@torque>
2023-08-31 13:58:04 +0100

    Updated code with detailed comments

--- get_many-body_clusters.py
+++ get_many-body_clusters.py
@@ -3,6 +3,7 @@ import sys
 import os
 from collections import OrderedDict
 
+# Function to read the contents of a file
 def read_file(filename):
     try:
         with open(filename, 'r') as file:
@@ -12,36 +13,45 @@ def read_file(filename):
         print(f"File '{filename}' not found.")
         return None
 
+# Function to check if atom indices are valid
 def check_atom_indices(atom_indices, total_atoms):
+    # Check for duplicate atom indices
     if len(atom_indices) != len(set(atom_indices)):
         print("Error: Atom index is repeated in the monomers file.")
         return False
+    # Check if atom index is within the valid range
     if any(atom_index > total_atoms for atom_index in atom_indices):
         print("Error: Atom index exceeds the number of atoms defined in the CONTCAR file.")
         return False
     return True
 
+# Function to process monomers data
 def process_monomers(contcar_content, monomers_content):
+    # Split monomers content into lines and parse atom indices
     monomer_lines = monomers_content.strip().split('\n')
-    #monomers = [list(map(int, line.split(','))) for line in monomer_lines]
     monomers = [sorted(map(int, line.split(','))) for line in monomer_lines]
 
+    # Get total number of atoms from CONTCAR file
     total_atoms = sum(map(int, contcar_content.split('\n')[6].split()))
     all_atom_indices = list(range(1, total_atoms + 1))
 
+    # Check validity of atom indices
     if not check_atom_indices([atom for monomer in monomers for atom in monomer], total_atoms):
         return None
 
+    # Identify missing atoms in the monomers
     all_monomer_indices = [atom for monomer in monomers for atom in monomer]
     missing_atoms = list(set(all_atom_indices) - set(all_monomer_indices))
     monomers.append(missing_atoms)
 
     return monomers
 
+# Function to get atomic symbols from atom indices
 def get_atomic_symbols(contcar_content, atom_indices):
     atom_symbols = contcar_content.split('\n')[5].split()
     atomic_symbols = []
 
+    # Map atom indices to atomic symbols
     for atom_index in atom_indices:
         sum_integers = 0
         for i, num in enumerate(map(int, contcar_content.split('\n')[6].split())):
@@ -52,11 +62,13 @@ def get_atomic_symbols(contcar_content, atom_indices):
 
     return atomic_symbols
 
+# Function to create monomer CONTCAR file
 def create_monomer_contcar_file(file_name, contcar_content, monomer):
     with open(file_name, "w") as file:
         atomic_symbols = get_atomic_symbols(contcar_content, monomer)
         unique_atomic_symbols = list(OrderedDict.fromkeys(atomic_symbols))
 
+        # Write atomic symbols and other metadata to the new file
         file.write('   ' + '   '.join(unique_atomic_symbols) + '\n')
         file.write(contcar_content.split('\n')[1] + '\n')
         file.write('\n'.join(contcar_content.split('\n')[2:5]) + '\n')
@@ -64,15 +76,18 @@ def create_monomer_contcar_file(file_name, contcar_content, monomer):
         file.write('   ' + '   '.join(map(str, [atomic_symbols.count(symbol) for symbol in unique_atomic_symbols])) + '\n')
         file.write('\n'.join(contcar_content.split('\n')[8:9]) + '\n')
 
+        # Write atom coordinates for the monomer
         for atom_index in monomer:
             atom_line = contcar_content.split('\n')[8 + atom_index]
             file.write(atom_line + '\n')
 
+# Function to create pair combination CONTCAR file
 def create_pair_combination_contcar_file(file_name, contcar_content, monomer1, monomer2):
     with open(file_name, "w") as file:
         atomic_symbols = get_atomic_symbols(contcar_content, monomer1 + monomer2)
         unique_atomic_symbols = list(OrderedDict.fromkeys(atomic_symbols))
 
+        # Write atomic symbols and other metadata to the new file
         file.write('   ' + '   '.join(unique_atomic_symbols) + '\n')
         file.write(contcar_content.split('\n')[1] + '\n')
         file.write('\n'.join(contcar_content.split('\n')[2:5]) + '\n')
@@ -80,11 +95,14 @@ def create_pair_combination_contcar_file(file_name, contcar_content, monomer1, m
         file.write('   ' + '   '.join(map(str, [atomic_symbols.count(symbol) for symbol in unique_atomic_symbols])) + '\n')
         file.write('\n'.join(contcar_content.split('\n')[8:9]) + '\n')
 
+        # Write atom coordinates for the combined monomer
         for atom_index in monomer1 + monomer2:
             atom_line = contcar_content.split('\n')[8 + atom_index]
             file.write(atom_line + '\n')
 
+# Main function
 def main():
+    # Check command line arguments
     if len(sys.argv) == 1:
         contcar_filename = "CONTCAR"
         monomers_filename = "monomers"
@@ -96,9 +114,11 @@ def main():
         print(f"Usage: {script_name} [CONTCAR_filename monomers_filename]")
         return
 
+    # Read CONTCAR and monomers content
     contcar_content = read_file(contcar_filename)
     monomers_content = read_file(monomers_filename)
 
+    # Process monomers and create files
     if contcar_content is not None and monomers_content is not None:
         monomers = process_monomers(contcar_content, monomers_content)
 
@@ -109,5 +129,6 @@ def main():
                     combined_monomer = sorted(monomer1 + monomer2)
                     create_monomer_contcar_file(f"POSCAR_{i}_{j}", contcar_content, combined_monomer)
 
+# Run the main function if the script is executed directly
 if __name__ == "__main__":
     main()
